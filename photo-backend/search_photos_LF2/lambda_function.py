e440a179c04e61a908a22a086074ac932b8d2bab
samrudhibaldota <100147349+samrudhibaldota@users.noreply.github.com>
2025-11-30 14:03:10 -0500

    Update lambda_function.py

--- photo-backend/search_photos_LF2/lambda_function.py
+++ photo-backend/search_photos_LF2/lambda_function.py
@@ -166,5 +166,5 @@ def lambda_handler(event, context):
         "headers": {"Content-Type": "application/json"},
         "body": json.dumps(body),
     }
-print("Pipeline auto-trigger test")
+print("Pipeline auto-trigger test remove")
 
